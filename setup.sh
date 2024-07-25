04869d6acff865a5e7ba36c91eb96c211a503ef2
minjune song <you@example.com>
2024-07-25 07:25:10 +0000

    eva added

--- setup.sh
+++ setup.sh
@@ -27,9 +27,11 @@ echo "Sobumodule setup complete."
 
 pip install -U Pillow
 
-pip install scipy wandb datasets nvitop deepspeed matplotlib python-dotenv torchinfo cython 
+pip install scipy wandb datasets nvitop deepspeed matplotlib python-dotenv torchinfo cython torchvision==0.14.0 timm ftfy apex
 
 pip install -U pycocotools
+
+pip install xformers==0.0.22.post4 --index-url https://download.pytorch.org/whl/cu118
 
 # Set environment variables
 export HF_DATASETS_CACHE="/workspace/cache" WANDB_API_KEY="d9754de6924c1222712c0cd6cc680271a1183e4b" COCO_PATH='/workspace/coco' HF_API_KEY='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'
--- t.py
+++ t.py
@@ -4,37 +4,8 @@ import sys, os
 eva_path = os.path.join(os.path.dirname(__file__), 'EVA/EVA-CLIP/rei')
 sys.path.append(eva_path)
 
-from eva_clip import create_model_and_transforms, get_tokenizer, _build_vision_tower
+from eva_clip import create_model_and_transforms, get_tokenizer
 from PIL import Image
-
-# TODO load vision tower and preporcesser directly, no text enc
-@dataclass
-class CLIPVisionCfg:
-    layers: Union[Tuple[int, int, int, int], int] = 12
-    width: int = 768
-    head_width: int = 64
-    mlp_ratio: float = 4.0
-    patch_size: int = 16
-    image_size: Union[Tuple[int, int], int] = 224
-    ls_init_value: Optional[float] = None  # layer scale initial value
-    patch_dropout: float = 0. # what fraction of patches to dropout during training (0 would mean disabled and no patches dropped) - 0.5 to 0.75 recommended in the paper for optimal results
-    global_average_pool: bool = False # whether to global average pool the last embedding layer, instead of using CLS token (https://arxiv.org/abs/2205.01580)
-    drop_path_rate: Optional[float] = None  # drop path rate
-    timm_model_name: str = None  # a valid model name overrides layers, width, patch_size
-    timm_model_pretrained: bool = False  # use (imagenet) pretrained weights for named model
-    timm_pool: str = 'avg'  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
-    timm_proj: str = 'linear'  # linear projection for timm model output ('linear', 'mlp', '')
-    timm_proj_bias: bool = False  # enable bias final projection
-    eva_model_name: str = None # a valid eva model name overrides layers, width, patch_size
-    qkv_bias: bool = True
-    fusedLN: bool = False
-    xattn: bool = False
-    postnorm: bool = False
-    rope: bool = False
-    pt_hw_seq_len: int = 16   # 224/14
-    intp_freq: bool = False
-    naiveswiglu: bool = False
-    subln: bool = False
 
 model_name = "EVA02-CLIP-B-16" 
 pretrained = "/workspace/vlm/EVA02_CLIP_B_psz16_s8B.pt" # or "/path/to/EVA02_CLIP_B_psz16_s8B.pt"
@@ -43,19 +14,23 @@ image_path = "/workspace/vlm/cat.jpeg"
 caption = ["a diagram", "a dog", "a cat"]
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, _, preprocess = create_model_and_transforms(model_name, pretrained, force_custom_clip=True)
+
+model, _, preprocess = create_model_and_transforms(
+    model_name, 
+    pretrained, 
+    force_custom_clip=True,
+    pretrained_text='',
+    skip_list=['text']
+    )
+
 tokenizer = get_tokenizer(model_name)
 model = model.to(device)
 
 image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-text = tokenizer(["a diagram", "a dog", "a cat"]).to(device)
 
 with torch.no_grad(), torch.cuda.amp.autocast():
     image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
     image_features /= image_features.norm(dim=-1, keepdim=True)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
 
-    text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+print(iamge_features.shape)
 
-print("Label probs:", text_probs)  # prints: [[0.8275, 0.1372, 0.0352]]
